006d054810b29897af72e867e5d2345a1832fa93
Alas-3 <87595218+Alas-3@users.noreply.github.com>
2025-02-08 12:50:43 +0800

    FIXED: Cache and fetch of data for aired eps

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import { useEffect } from "react"; // Import useEffect from React
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Clear the cache when the app initializes
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem('recentEpisodesData');
+      localStorage.removeItem('recentAnimeData');
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
